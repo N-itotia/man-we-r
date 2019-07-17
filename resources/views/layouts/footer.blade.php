81c7fe14532d0dfc5ffad9b2a15bcf6e341b4052
Nigel <nigelnjagi@gmail.com>
2019-07-17 15:01:22 +0300

    work
    .

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,6 @@
+	<div style="width: 100% ;position: fixed; height: 40px;background-color:white;bottom:0px;border-top:1px solid lightgray " >
+		<span style="float:left; margin:10px"><i class="fa fa-copyright" aria-hidden="true"></i>Created by Projekt-kovu API 2019 </span>
+		<span style="float:right; margin:10px">
+  		</span>
+
+	</div>
